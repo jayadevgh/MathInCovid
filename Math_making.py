4846b148fa3e6f5c10588c23dfbb503bd5b8ce44
Kavitha Vanamala <Kvanamala@Kavithas-MacBook-Pro.local>
2020-07-30 15:23:58 -0700

    Math making edit

--- Math_making.py
+++ Math_making.py
@@ -1,14 +1,72 @@
-print("Hello")
+# importing libraries or modules may not needed
 
-def amazing(start, end):
-    if start == end:
-        print("they are the same number")
-    elif start > end:
-        print("start is bigger than end")
-    else:
-        print("end is bigger than start")
+# randomize questions or import scanner
 
-inputs = input("What is your name")
 
-if inputs == "Bob":
-    print("Hi Bob, how are you guys doing ")
+#_____________________________________________________________________________________________________
+# Add this code with the rest and make sure it works properly in the website - (Do it when everyone is in the meeting together)
+
+pr1 = input("5x - 4 + 3 + x = 9 - 4 (find x) ")
+if pr1 == '1' or pr1 == '1.0':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 1")
+
+pr2 = input("3x + 4 - 1 = 12 (find x) ")
+if pr2 == '3' or pr2 == '3.0':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 3")
+
+pr3 = input("16 - 2x = 5x + 9 (find x) ")
+if pr3 == '1' or pr3 == '1.0':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 1")
+
+pr4 = input("x + 5 = 10 (find x) ")
+if pr4 == '5' or pr4 == '5':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 5")
+
+pr5 = input("x * 4 = 24 (find x)")
+if pr5 == '6' or pr5 == '6':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 6")
+
+pr6 = input("2(x - 1) + 8 = 4x - 20 (find x) ")
+if pr6 == '13' or pr6 == '13.0':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 13")
+
+pr7 = input("3x + 2x - 2 = 23 (find x) ")
+if pr7 == '5' or pr7 == '5.0':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 5")
+
+pr8 = input("3(9 + 3) + 2x = 36 (find x) ")
+if pr8 == '0' or pr8 == '0.0':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 0")
+
+pr9 = input("6 - 2(3 - x) = 10 (find x) ")
+if pr9 == '5' or pr9 == '5':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 5")
+
+pr10 = input("2x + 5 = 15")
+if pr10 == '10' or pr10 == '10.0':
+    print("Correct, you got the right answer!")
+else:
+    print("incorrect, answer is 10")
+
+
+# The button is going to be in javascript (it is easier to code)
+#________________________________________________________________________________________-
+
