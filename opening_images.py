b8fdce2d5f8bba5aaf0a0421c894bd6cf0b9678f
sidvanamala <68829721+sidvanamala@users.noreply.github.com>
2020-07-31 09:29:42 -0700

    Add files via upload
    here is code

---
+++ opening_images.py
@@ -0,0 +1,15 @@
+from PIL import Imageg
+
+# open method used to open different extension image file
+# put your image file name below
+im = Image.open('')
+
+# Now we have to show the image
+
+im.show()
+
+'''
+I will put a picture into the website using python
+You all should use html to create a url to access the video content of the lesson
+
+'''
